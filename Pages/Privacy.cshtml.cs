6513eee81007fedb30202f7ad98a5362cd9880d5
nikita.prigolovkin <vitalii.prigolovkin@gmail.com>
2024-02-03 18:25:19 +0300

    update(03.02.2024)

--- Pages/Privacy.cshtml.cs
+++ Pages/Privacy.cshtml.cs
@@ -6,14 +6,16 @@ namespace DZ2_31._01._2024_.Pages
 	public class PrivacyModel : PageModel
 	{
 		private readonly ILogger<PrivacyModel> _logger;
+		public AnimalViewModel AnimalViewModel = new AnimalViewModel();
 
-		public PrivacyModel(ILogger<PrivacyModel> logger)
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
 		{
 			_logger = logger;
 		}
 
 		public void OnGet()
 		{
-		}
-	}
+        }
+    }
 }
